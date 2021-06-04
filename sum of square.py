f52e480a32f3b769fa9247316c21dd38002ec254
Dishari Mondal <84579983+missdishari@users.noreply.github.com>
2021-06-04 19:57:14 +0530

    Add files via upload

---
+++ sum of square.py
@@ -0,0 +1,22 @@
+# # for sum of square of first n natural numbers
+# n=int(input("enter to get sum of square of numbers upto -"))
+# i=1
+# sum=0
+# while i<=n:
+#     mul=i*i
+#     sum=sum+mul
+#     i=i+1
+# print("the sum of first",n,"natural numbers is",sum)
+
+
+# for sum of cube of first n natural numbers
+n=int(input("enter to get sum of square of numbers upto -"))
+i=1
+sum=0
+while i<=n:
+    mul=i*i*i
+    sum=sum+mul
+    i=i+1
+print("the sum of first",n,"natural numbers is",sum)
+
+
