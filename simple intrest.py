fc39dfd529f6a8b32bcceb7f43e9a43b7de57245
Dishari Mondal <84579983+missdishari@users.noreply.github.com>
2021-06-04 19:56:57 +0530

    Add files via upload

---
+++ simple intrest.py
@@ -0,0 +1,5 @@
+prin_val = int(input("enter the principal value-"))
+rate = int(input("enter the rate of intrest -"))
+time_period = int(input("enter the time period in years-"))
+simple_intrest = (prin_val*rate*time_period)/100
+print("simple intrest -",simple_intrest)
